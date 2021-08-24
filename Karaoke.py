a04fe33d41e1b1950defe8610ae9023b37b21f07
CDGDG <77666879+CDGDG@users.noreply.github.com>
2021-08-24 13:45:41 +0900

    Add files via upload

--- Karaoke.py
+++ Karaoke.py
@@ -12,12 +12,12 @@ from PyQt5 import QtWebEngineWidgets
 from PyQt5 import QtWebEngineCore
 from PyQt5.QtWebEngineWidgets import QWebEngineSettings
 
-main_ui = "./karaoke_main.ui"
+main_ui = "karaoke_main.ui"
 popular_ui = "./karaoke_popular.ui"
 
 popular_site = 'https://www.tjmedia.co.kr/tjsong/song_monthPopular.asp'
 recent_site = 'http://www.tjmedia.co.kr/tjsong/song_monthNew.asp'
-search_site = 'http://www.tjmedia.co.kr/tjsong/song_search.asp'
+search_site = 'https://www.tjmedia.co.kr/tjsong/song_search_list.asp?strType={}&strText={}&strCond=1&strSize02=100&intPage={}'
 
 DEVELOPER_KEY = "AIzaSyCxCQR3XfJdM13CzN3IIEQOuiSPq6ocoRg"
 YOUTUBE_API_SERVICE_NAME="youtube"
@@ -42,15 +42,17 @@ class Karaoke(QMainWindow):
         self.popular_btn.clicked.connect(self.move_popular)
         self.recent_btn.clicked.connect(self.move_recent)
         self.title_btn.clicked.connect(self.move_title)
-        self.singer_btn.clicked.connect(self.move_singer)
+        self.singer_btn.clicked.connect(lambda : self.move_singer())
         self.my_btn.clicked.connect(self.move_my)
 
         self.return_btn.clicked.connect(self.return_main)
         self.return_btn_2.clicked.connect(lambda : self.stackedWidget.setCurrentWidget(self.main_page))
         self.return_btn_3.clicked.connect(lambda : self.stackedWidget.setCurrentWidget(self.main_page))
         self.return_btn_4.clicked.connect(lambda : self.stackedWidget.setCurrentWidget(self.main_page))
+        self.return_btn_5.clicked.connect(lambda : self.stackedWidget.setCurrentWidget(self.main_page))
 
-        self.title_edit.returnPressed.connect(self.enter)
+        self.title_edit.returnPressed.connect(lambda : self.make_search(1,self.title_edit.text(),self.title_frame))
+        self.singer_edit.returnPressed.connect(lambda : self.make_search(2,self.singer_edit.text(),self.singer_frame))
 
 
     def make_popular(self):
@@ -77,9 +79,35 @@ class Karaoke(QMainWindow):
         for i, song in enumerate(self.recent_list):
             self.recent_frame.layout().addWidget(self.make_frame(song.number, song))
 
-    def return_main(self):
-        self.youtube_view.setUrl(QUrl('none'))
-        self.stackedWidget.setCurrentWidget(self.main_page)
+    def make_search(self,type1,text,frame):
+        for c in frame.children():
+            if str(type(c)) == "<class 'PyQt5.QtWidgets.QFrame'>":
+                c.close()
+
+        i=1
+        flag = False
+        while True:
+            target = search_site.format(type1,text,i)
+            rq = requests.get(target)
+            rq.encoding = 'utf-8'
+            results = BeautifulSoup(rq.text,'html.parser').select('tbody > tr')
+            if len(results)==0:
+                break;
+            for result in results:
+                r = result.text.strip().split("\n")
+                if r[0]=="곡 번호 ":
+                    continue
+                elif "검색결과를 찾을수 없습니다.".__eq__(r[0]):
+                    flag = True
+                    break;
+                elif len(r) > 2:
+                    song = Song(r[0],r[1],r[2])
+                    frame.layout().addWidget(self.make_frame(song.number, song))
+            if flag:
+                break;
+            i+=1
+
+
 
     def move_popular(self):
         print('popular')
@@ -91,26 +119,35 @@ class Karaoke(QMainWindow):
 
     def move_title(self):
         print('title')
+        self.make_search(1, "", self.title_frame)
         self.stackedWidget.setCurrentWidget(self.title_page)
 
-    def move_singer(self):
-        pass
+    def move_singer(self,singer=""):
+        print('singer')
+        self.make_search(2, singer, self.singer_frame)
+        if singer!="":
+            self.singer_edit.setText(singer)
+        self.stackedWidget.setCurrentWidget(self.singer_page)
+
+    def return_main(self):
+        self.youtube_view.setUrl(QUrl('none'))
+        self.stackedWidget.setCurrentWidget(self.main_page)
 
     def move_my(self):
         pass
 
-    def enter(self):
-        rq = requests.get(search_site).text
-
     def make_frame(self,number,song):
-        newFrame = QFrame();newFrame.setStyleSheet('QFrame{background-color:None;border: 2px solid rgb(255,255,255);}')
+        newFrame = QFrame();newFrame.setStyleSheet('QFrame{background-color:none;border: 2px solid rgb(255,255,255);}')
         newLayout = QHBoxLayout(newFrame);newLayout.setContentsMargins(0,0,0,0);newLayout.setSpacing(0)
-        numberLabel = QLabel();numberLabel.setStyleSheet('background-color:None;border:None;')
+        numberLabel = QLabel();numberLabel.setStyleSheet('background-color:none;border:none;')
         numberLabel.setText(str(number));numberLabel.setFont(QFont('210 옴니고딕 040',20))
         songButton = QPushButton();
         songButton.setText(song.title if len(song.title) < 30 else song.title[:30]);songButton.setFont(QFont('210 옴니고딕 040',15));songButton.clicked.connect(lambda :song.play(self.stackedWidget,self.play_page,self.youtube_view))
+        songButton.setStyleSheet('background-color:none;border:none;')
         singerButton = QPushButton()
         singerButton.setText(song.singer if len(song.singer) < 16 else song.singer[:16]);singerButton.setFont(QFont('210 옴니고딕 040',15))
+        singerButton.setStyleSheet('background-color:none;border:none;')
+        singerButton.clicked.connect(lambda: self.move_singer(song.singer))
         newLayout.addWidget(numberLabel,2,Qt.AlignCenter)
         newLayout.addWidget(songButton,10)
         newLayout.addWidget(singerButton,5)
